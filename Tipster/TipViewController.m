c772790bd8a5dab91de47d73364e4bf4084d46a6
elisaj1317 <41582626+elisaj1317@users.noreply.github.com>
2021-06-22 16:23:27 -0700

    Show Labels and initial Settings Page

--- Tipster/TipViewController.m
+++ Tipster/TipViewController.m
@@ -13,14 +13,15 @@
 @property (weak, nonatomic) IBOutlet UILabel *totalLabel;
 @property (weak, nonatomic) IBOutlet UISegmentedControl *tipPercentControl;
 @property (weak, nonatomic) IBOutlet UIView *labelsContainerView;
+@property (strong, nonatomic) IBOutlet UIView *backgroundView;
 
 @end
 
 @implementation TipViewController
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    [self hideLabels];
+    [self changeColorOriginal];
 }
 
 - (IBAction)onTap:(id)sender {
@@ -30,12 +31,20 @@
 }
 
 - (IBAction)updateLabels:(id)sender {
+    [self changeColorTip];
+    if (self.labelsContainerView.alpha == 0) {
+        [self showLabels];
+    }
+    
     if (self.billField.text.length == 0) {
         [self hideLabels];
+        [self changeColorOriginal];
     }
     
     
-    double tipPercentages[] = {0.15, 0.2, 0.25};
+    
+    
+    double tipPercentages[] = {0.10, 0.15, 0.20};
     double tipPercentage = tipPercentages[self.tipPercentControl.selectedSegmentIndex];
     
     double bill = [self.billField.text doubleValue];
@@ -61,6 +70,50 @@
         self.labelsContainerView.alpha = 0;
     }];
     
+}
+
+- (void)changeColorOriginal {
+    self.backgroundView.backgroundColor = [UIColor
+                                           colorWithRed: 0.69
+                                           green: 0.93
+                                           blue: 0.93
+                                           alpha: 1.0];
+}
+
+- (void)changeColorTip {
+    // change color to red
+    if (self.tipPercentControl.selectedSegmentIndex == 0) {
+        self.backgroundView.backgroundColor = [UIColor
+                                               colorWithRed: 1.0
+                                               green: 0.8
+                                               blue: 0.8
+                                               alpha: 1.0];
+    }
+    // change color to green
+    else {
+        self.backgroundView.backgroundColor = [UIColor
+                                               colorWithRed: 0.60
+                                               green: 1.0
+                                               blue: 0.60
+                                               alpha: 1.0];
+        
+    }
+}
+
+- (void)showLabels {
+    [UIView animateWithDuration:0.5 animations:^{
+        CGRect billFrame = self.billField.frame;
+        billFrame.origin.y -= 200;
+        
+        self.billField.frame = billFrame;
+        
+        CGRect labelsFrame = self.labelsContainerView.frame;
+        labelsFrame.origin.y -= 200;
+        
+        self.labelsContainerView.frame = labelsFrame;
+        
+        self.labelsContainerView.alpha = 1;
+    }];
 }
 
 
